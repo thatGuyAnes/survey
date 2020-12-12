9fb93214fe9e726c4abc289b80a7c3cef651673e
Anes-Marzooki <marzoukianas@gmail.com>
2020-12-12 16:28:19 +0100

    passport setup

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require( 'express' )
+const express = require( 'express' );
+const passport = require( 'passport' );
+const GoogleOath = require( 'passport-google-oauth20' );
 
 const app = express();
+passport.use( GoogleOath(  ) );
 
-app.get('/', ( req, res ) => {
-		res.send( { hi: 'there' } );
-	});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
